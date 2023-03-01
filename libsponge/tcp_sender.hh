9f71b45c401d17f7987e6ac9ca1bed1ad0e1fe3c
SonglinLife <wusonglin@tongji.edu.cn>
2023-03-01 17:09:34 +0800

    Done lab4! add tcp connection, what a awesome masterpiece!

--- libsponge/tcp_sender.hh
+++ libsponge/tcp_sender.hh
@@ -132,6 +132,10 @@ class TCPSender {
     //! \brief relative seqno for the next byte to be sent
     WrappingInt32 next_seqno() const { return wrap(_next_seqno, _isn); }
     //!@}
+    //
+    bool eof() const {
+        return (bytes_in_flight() == 0 && _stream.eof() && _next_seqno == _stream.bytes_written() + 2);
+    }
 };
 
 #endif  // SPONGE_LIBSPONGE_TCP_SENDER_HH
